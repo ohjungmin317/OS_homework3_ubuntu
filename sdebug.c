c6ea1135bd6add1f1b47a02c293046705704072a
ohjungmin <root@ip-172-31-85-131.ec2.internal>
2022-10-14 13:14:08 +0000

    homework3

--- sdebug.c
+++ sdebug.c
@@ -3,15 +3,13 @@
 #include "user.h"
 
 #define PNUM 5 // number of process 
-#define PRINT_CYCLE 100000000 // process prints information cycle
+#define PRINT_CYCLE 10000000 // process prints information cycle
 #define TOTAL_COUNTER 500000000 // terminate the process counter
 
 void sdebug_func(void){
     int n, pid;
-
+    int weight = 1; // use the weightset systemcall for weight value increase
     int start, end; // calculate uptime() value -> start & end 
-    
-    int weight = 1; // use the weightset systemcall for weight value increase 1
 
     printf(1, "start sdebug command\n");
 
@@ -20,45 +18,58 @@ void sdebug_func(void){
 
         if (pid < 0){ // exception for fork error when pid < 0 
             printf(1, "fork error\n");
+            break;
         }
         if (pid == 0){ // when pid == 0 child process fork() return value 0 so only execute child process 
-            long counter;
+            
+            start = uptime(); // start the uptime() -> when execute process clock_tick -> + 1
+            
+            long counter=0;
+            int first = 1;
+
+            int print_counter = PRINT_CYCLE;  
 
             weightset(weight); // child process execute weightset => + 1 
 
-            start = uptime(); // start the uptime() -> when execute process clock_tick -> + 1 
+            while (counter++<=TOTAL_COUNTER) {  // loop for the counter for TOTAL_COUNTER => when counter == TOTAL_COUNTER process terminated
+                
+                print_counter--;
 
-            for (counter = 0; counter < TOTAL_COUNTER; counter++){ // loop for the counter for TOTAL_COUNTER => when counter == TOTAL_COUNTER process terminated
-                if (counter == PRINT_CYCLE){ // counter == PRINT_CYCLE print process info
-                    
-                    end = uptime(); // end the uptime() record the process running time 
-
-                    int totaltime = (end - start) * 10; // (process print info time - process start time) * 10 
-
-                    printf(1, "PID : %d, WEIGHT : %d, TIMES : %d ms\n", getpid(), weight, totaltime);
+                if(print_counter == 0)
+                {
+                    if(first){
+                        end = uptime();
+                        int total_time = (end - start) * 10;
+                        printf(1,"PID: %d, WEIGHT: %d",getpid(),weight);
+                        printf(1,"TIMES: %d ms\n",total_time);
+                        first = 0;
+                    }
+                    print_counter = PRINT_CYCLE;
                 }
                 
             }
+            printf(1,"PID: %d terminated\n",getpid());
             exit();
         }
-        weight++;
-       
+        weight++;      
     }
+    
     /* wait for all child processes by sdebug.c create process to terminate */
     /* Refer to forktest.c */
-    if (n == pid){
-        printf(1, "fork claimed to work N times!\n", pid);
-        exit();
-    }
+
+    // if (n == pid){
+    //     printf(1, "fork claimed to work N times!\n", pid);
+    //     exit();
+    // }
 
     for (; n > 0; n--){
         if ((pid = wait() )< 0){
             printf(1, "wait stopped early\n");
             exit();
         }
-        else{
-            printf(1,"PID: %d terminated\n",pid);
-        }
+        // else{
+        //     printf(1,"PID: %d terminated\n",pid);
+        // }
     }
 
     if (wait() != -1){
